9027ca0a9172108c5f5e0b4c502be13fa4910b06
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2025-12-08 12:25:23 -0300

    adiciona PersistenceEntity

---
+++ src/main/java/br/com/ifba/sididoc/entity/PersistenceEntity.java
@@ -0,0 +1,31 @@
+package br.com.ifba.sididoc.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class PersistenceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+}
