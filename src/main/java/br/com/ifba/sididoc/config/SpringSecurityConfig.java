cd98d716d5d7208adf3799311a1e16fe7af44bf7
Gabriel Barauna <gabrielbarauna2016@gmail.com>
2025-12-13 16:04:58 -0300

    Fix: Rota de setores e upload no frontend

--- src/main/java/br/com/ifba/sididoc/config/SpringSecurityConfig.java
+++ src/main/java/br/com/ifba/sididoc/config/SpringSecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -27,6 +32,7 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -37,18 +43,35 @@ public class SpringSecurityConfig {
                                 "/auth/login",
                                 "/auth/complete-registration",
                                 "/auth/request-password-reset",
-                                "/auth/reset-password"
+                                "/auth/reset-password",
+                                "/sectors/find-all-active"
                         ).permitAll()
 
                         //Protecao de endpoints por role
                         .requestMatchers("/users/**").hasAnyRole("SUPER_ADMIN", "SECTOR_ADMIN")
                         .requestMatchers("/sectors/**").hasAnyRole("SUPER_ADMIN", "SECTOR_ADMIN")
+                        .requestMatchers("/documents/upload").authenticated()
 
                         .anyRequest().authenticated()
                 );
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 
     @Bean
