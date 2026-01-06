13b1034984c2b178549ffd21f96b41caaa0206a2
Rian <rian10cardosi@gmail.com>
2026-01-06 00:16:53 -0300

    Add: endpoint de busca de documentos

--- src/main/java/br/com/ifba/sididoc/repository/DocumentRepository.java
+++ src/main/java/br/com/ifba/sididoc/repository/DocumentRepository.java
@@ -2,7 +2,18 @@ package br.com.ifba.sididoc.repository;
 
 import br.com.ifba.sididoc.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+    @Query(value = """
+        SELECT * FROM documents d
+        WHERE d.search_vector @@ to_tsquery('portuguese', :termo)
+        ORDER BY ts_rank(d.search_vector, to_tsquery('portuguese', :termo)) DESC
+    """, nativeQuery = true)
+    List<Document> searchByTerm(@Param("termo") String termo);
 
 }
--- src/main/java/br/com/ifba/sididoc/web/controller/DocumentController.java
+++ src/main/java/br/com/ifba/sididoc/web/controller/DocumentController.java
@@ -1,9 +1,11 @@
 package br.com.ifba.sididoc.web.controller;
 
+import br.com.ifba.sididoc.entity.Document;
 import br.com.ifba.sididoc.service.DocumentService;
 import br.com.ifba.sididoc.web.dto.DocumentResponseDTO;
 import br.com.ifba.sididoc.web.dto.UploadDocumentDTO;
 import jakarta.validation.Valid;
+import net.sourceforge.tess4j.TesseractException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/documents")
@@ -30,7 +33,7 @@ public class DocumentController {
         try {
             DocumentResponseDTO response = documentService.uploadDocument(dto);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        } catch (IOException e) {
+        } catch (IOException | TesseractException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -39,5 +42,11 @@ public class DocumentController {
     public ResponseEntity<Page<DocumentResponseDTO>> findAll(@PageableDefault(size = 24, sort = "uploadDate", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<DocumentResponseDTO> documents = documentService.findAll(pageable);
         return ResponseEntity.ok(documents);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Document>> search(@RequestParam("q") String query) {
+        List<Document> results = documentService.search(query);
+        return ResponseEntity.ok(results);
     }
 }
